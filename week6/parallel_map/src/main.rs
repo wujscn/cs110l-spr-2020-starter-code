1dee1f1e575ff752daa79fb7341e643d1ed92b8d
wujs <wujscn@foxmail.com>
2022-07-22 23:43:50 +0800

    Week 6 Exercises: Sharing Data by Communicating

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -8,7 +8,34 @@ where
     U: Send + 'static + Default,
 {
     let mut output_vec: Vec<U> = Vec::with_capacity(input_vec.len());
-    // TODO: implement parallel map!
+    output_vec.resize_with(input_vec.len(), Default::default);
+    let (sender , receiver) = crossbeam_channel::unbounded::<(T, usize)>();
+    let (rst_sender, rst_receiver) = crossbeam_channel::unbounded::<(U, usize)>();
+    let mut threads = Vec::new();
+    for _ in 0..num_threads {
+        let receiver = receiver.clone();
+        let rst_sender = rst_sender.clone();
+        threads.push(thread::spawn(move || {
+            while let Ok(next_item) = receiver.recv() {
+                rst_sender.send((f(next_item.0), next_item.1))
+                        .expect("Tried writing to channel, but there are no receivers!");
+            }
+        }));
+    }
+    let mut idx = input_vec.len();
+    while let Some(item) = input_vec.pop() {
+        idx -= 1;
+        sender.send((item, idx))
+            .expect("Tried writing to channel, but there are no receivers!");
+    }
+    drop(sender);
+    drop(rst_sender);
+    while let Ok((item, idx)) = rst_receiver.recv() {
+        output_vec[idx] = item;
+    }
+    for thread in threads {
+        thread.join().expect("Panic occurred in thread");
+    }
     output_vec
 }
 
